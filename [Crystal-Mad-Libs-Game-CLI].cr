7bbe0c72a7404a28e6db132a14a2cbb8da42c826
Mohammad Babaee <79867893+mohammad-babaee@users.noreply.github.com>
2022-09-13 15:01:22 -0400

    Add files via upload

---
+++ [Crystal-Mad-Libs-Game-CLI].cr
@@ -0,0 +1,35 @@
+puts ("Hello , Welcome to the Mad Libs Game !")
+
+puts ("
+    Version : 1.1.1
+    ---------
+    programming Language : Crystal
+    ---------
+    Developer : Mohammad Babaee")
+
+puts (" --------------------------- ")
+
+puts ("   Please Enter A Color : ")
+
+color = gets
+
+puts ("   Please Enter A Celebrity Name : ")
+
+celeb = gets
+
+puts ("   Please Enter A plural Noun : ")
+
+plural_noun = gets
+
+
+puts ("  |------------------------- ")
+puts("      M A D L I B S G A M E ")
+puts ("  | ------------------------")
+puts ("  | Roses Are #{color}")
+puts ("  | #{plural_noun} Are Blue")
+puts ("  | I LOVE #{celeb}")
+puts("  | ------------------------")
+
+puts ("   --- Have Fun ! ---")
+
+puts("------ This App Is Published Under (MIT License) ------")
